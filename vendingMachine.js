dfd5676f50f0d52cba5b4055a4d4fd764edce47f
echumphry <36779709+echumphry@users.noreply.github.com>
2020-02-04 22:45:35 -0800

    Add files via upload
    Added class transaction, function for  cancel, got coin buttons to add up and display total. Updated html id's to work with new javascript.

--- vendingMachine.js
+++ vendingMachine.js
@@ -1,4 +1,37 @@
 
+class Transaction {
+  constructor() {
+    this.total = 0;
+    alert("a new transaction has started");
+    
+    
+  }
+  addNickel() {
+
+    this.total += .05;
+    
+  }
+
+  addDime() {
+
+    this.total += .10;
+  
+  }
+
+  addQuarter(){
+
+    this.total += .25;
+
+  }
+
+  addDollar(){
+
+    this.total += 1.00;
+  
+
+  }
+}
+
 
 var transactionTotal = 0;  
 
@@ -6,121 +39,85 @@ mainDisplay(transactionTotal);
 cashModalDisplay(transactionTotal);
 
 
-class insertCash {
-  constructor() {
-  	this.total = 0;
-    this.nickel = .05;
-    this.dime = .10;
-    this.quarter = .25;
-    this.dollar = 1.00;
-  }
-  addNickel() {
 
-  	this.total = this.total + this.nickel;
-    return this.total; 
-  }
 
-  addDime() {
 
-  	this.total = this.total + this.dime;
-    return this.total; 
-  }
-
-  addQuarter(){
-
-  	this.total = this.total + this.quarter;
-  	return this.total;
-
-  }
-
-  addDollar(){
-
-  	this.total = this.total + this.quarter;
-  	return this.total;
-
-  }
-}
-
-// var myTransaction = new insertCash();
-//  myTransaction.addDollar;
-//  myTransaction.addQuarter;
-//  myTransaction.total = myTransaction.quarter  +  myTransaction.dollar;
-//  alert(myTransaction.total);
-
+// func startCashTransaction runs when the coin button is pushed
+// it creates a new object(transaction) where the user can click on 
+// the coin buttons adding to the total. 
 
 function startCashTransaction(){
 
-	var myTransaction = new insertCash();
+var myTransaction = new Transaction();
+
+    $('#nickel').click(function(){
+
+        myTransaction.addNickel();
+        cashModalDisplay(myTransaction.total);
+
+  
+    });
+
+    $('#dime').click(function(){
+
+      myTransaction.addDime();
+      cashModalDisplay(myTransaction.total);
+     
+    });
+
+     $('#quarter').click(function(){
+
+      myTransaction.addQuarter();
+      cashModalDisplay(myTransaction.total);
+     
+    });
+
+     $('#dollar').click(function(){
+
+      myTransaction.addDollar();
+      cashModalDisplay(myTransaction.total);
+
+    });
+
+    $('#coinClose').click(function(){
+
+      myTransaction.total = 0;
+      cashModalDisplay(myTransaction.total);
+
+    }); 
+
+    $('#coinOk').click(function(){
+
+      cashModalDisplay(myTransaction.total);
+      return mainDisplay(myTransaction.total);
+
+    }); 
+
+  }
 
 
-	alert("start cash transaction funct works");
+  function cancelReturn(){
 
-
-	myTransaction = $('#nickel').click(function(){
-     addNickel()
-	});
-
-	myTransaction = $('#dime').click(function(){
-     addDime()
-	});
-
-	myTransaction = $('#quarter').click(function(){
-     addQuarter()
-	});
-
-	myTransaction = $('#dollar').click(function(){
-     addDollar()
-	});
-
-	
-	//alert("Transaction Started, value is: " + myTransaction.total)
-
-	
-  // Retrieve
-  //document.getElementById("result").innerHTML = sessionStorage.getItem("storedTransaction");
-  //converts strings back to numbers
-  // var = Number(checking.amount);  
+    var amount = document.getElementsByClassName("amountContainer")[0].innerHTML;
+    alert( amount+" was returned");
+    amount = 0;
+    mainDisplay(amount);
+   
 
 }
 
 
-function addNickel(){  
+  function currencyFormat(amount){
 
-alert("made it to addNickel function") 
-
-
-}
-
-function addDime(){
- 
- alert("made it to addDime function") 
-	
-}
-
-function addQuarter(){
-
-alert("made it to addQuarter function") 
-	
-}
-
-function addDollar(){
-
-alert("made it to addDollar function") 
-	
-}
-
-
-function currencyFormat(amount){
-
-	const formatter = new Intl.NumberFormat('en-US', {
+  const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2
-  })	
+  })  
 
-	amount = formatter.format(amount);
+  amount = formatter.format(amount);
 
-	return amount;
+  return amount;
 
 }
 
@@ -131,47 +128,120 @@ function mainDisplay(total){
 
 
 
-		const formatter = new Intl.NumberFormat('en-US', {
+    const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2
-  })	
+  })  
 
 
 
-  	total = formatter.format(total);
+    total = formatter.format(total);
 
- 	document.getElementsByClassName("amountContainer")[0].innerHTML = total;
- 	
+  document.getElementsByClassName("amountContainer")[0].innerHTML = total;
+  
 
 
 }
 
 function cashModalDisplay(amount){
 
-	const formatter = new Intl.NumberFormat('en-US', {
+  const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2
-  })	
+  })  
 
 
 
-  	amount = formatter.format(amount);
+    amount = formatter.format(amount);
 
- 	document.getElementsByClassName("amountContainer")[1].innerHTML = amount;
+  document.getElementsByClassName("amountContainer")[1].innerHTML = amount;
 
- 	return amount;
-
-}
-
-
-function addCash(){
-
-
-
+  return amount;
 
 }
+
+
+
+
+
+
+	
+	
+  // Retrieve
+  //document.getElementById("result").innerHTML = sessionStorage.getItem("storedTransaction");
+  //converts strings back to numbers
+  // var = Number(checking.amount);  
+
+
+
+// function insertCoins(myTransaction){
+
+//   var thisTransaction;
+
+//   while (myTransaction != getElementById("coinClose"){
+
+//     thisTransaction = $('#nickel').click(function(){
+
+//       myTransaction.addNickel();
+
+  
+//     });
+
+//     thisTransaction = $('#dime').click(function(){
+
+//       myTransaction.addDime();
+     
+//     });
+
+//     thisTransaction = $('#quarter').click(function(){
+
+//       myTransaction.addQuarter();
+     
+//     });
+
+//     thisTransaction = $('#dollar').click(function(){
+
+//       myTransaction.addDime();
+
+//     });
+
+//   }
+
+
+// }
+
+
+// function addNickel(){  
+
+// alert("made it to addNickel function") 
+
+
+// }
+
+// function addDime(){
+ 
+//  alert("made it to addDime function") 
+	
+// }
+
+// function addQuarter(){
+
+// alert("made it to addQuarter function") 
+	
+// }
+
+// function addDollar(){
+
+// alert("made it to addDollar function") 
+	
+// }
+
+
+
+
+
 
 
 
